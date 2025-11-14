91d9b17715cbc0789dfd70fcca9c883bdd24ccdc
szbmrk <szmark16@gmail.com>
2025-11-14 17:52:42 +0100

    chore: add config.toml to gitignore

--- tawos/src/main.py
+++ tawos/src/main.py
@@ -1,13 +1,16 @@
+import os
 from core.converter import export_sql_to_csv
 from core.data_cleaning import (
     add_points_generated_by_local_model,
     remove_unnecessery_columns,
 )
 from core.log import get_logger
+from config_loader import config
 
 logger = get_logger("main")
 
 if __name__ == "__main__":
-    export_sql_to_csv()
+    if not os.path.exists(f"{config.EXPORT_FOLDER}/Issue.csv"):
+        export_sql_to_csv()
     remove_unnecessery_columns()
     add_points_generated_by_local_model()
